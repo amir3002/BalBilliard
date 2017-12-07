fd3e2907bdfc581c84b21c228934b1023bf1d779
amir3002 <amirg3002@gmail.com>
2017-12-07 18:17:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/amir/balbilliard/Registration_2.java
@@ -0,0 +1,13 @@
+package com.example.amir.balbilliard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registration_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_2);
+    }
+}
---
+++ app/src/main/java/com/example/amir/balbilliard/Reset_3.java
@@ -0,0 +1,13 @@
+package com.example.amir.balbilliard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Reset_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_3);
+    }
+}
